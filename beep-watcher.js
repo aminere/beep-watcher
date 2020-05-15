4b01a4d00696c161568cd0327415c3b23fd69582
Amine Rehioui <amine@franticsoftware.com>
2020-05-14 20:57:47 -0400

    Fix cwd() call

--- beep-watcher.js
+++ beep-watcher.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 'use strict';
 
-console.log(`Watching '${process.cwd}' for changes..`);
+console.log(`Watching '${process.cwd()}' for changes..`);
 
 // Plays a beep when a file changes in the current directory
 require("fs").watch('.', (event, file) => {
